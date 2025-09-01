aa335585ef0b4c546e4af86c40ecb6e529349cc3
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-01 15:30:03 -0300

    feat: realizando section de noticias e filtros

--- src/pages/PortalInicial.jsx
+++ src/pages/PortalInicial.jsx
@@ -21,6 +21,41 @@ export const PortalInicial = () => {
       <span>A</span>
     </div>
    </section>
+   <section className='flex bg-background justify-between p-10 gap-6 h-screen'>
+    <section>
+      filtros legais
+    </section>
+    <section className='flex flex-col justify-around'>
+<div className="bg-white rounded-xl shadow-lg p-6 w-80 h-64 flex flex-col items-center justify-center">
+  <h3 className="text-xl font-bold mb-2">
+    Título do Card
+  </h3>
+  <p className="text-gray-700 text-center mb-4">
+      AAAAAAAAAAAAAAAAAAAA
+  </p>
+
+</div>
+<div className="bg-white rounded-xl shadow-lg p-6 w-80 h-64 flex flex-col items-center justify-center">
+  <h3 className="text-xl font-bold mb-2">
+    Título do Card
+  </h3>
+  <p className="text-gray-700 text-center mb-4">
+    AAAAAAAAAAAAAAAAAAAA
+  </p>
+
+</div>
+<div className="bg-white rounded-xl shadow-lg p-6 w-80 h-64 flex flex-col items-center justify-center">
+  <h3 className="text-xl font-bold mb-2">
+    Título do Card
+  </h3>
+  <p className="text-gray-700 text-center mb-4">
+     AAAAAAAAAAAAAAAAAAAA
+  </p>
+
+</div>
+
+    </section>
+   </section>
     </div>
   )
 }
