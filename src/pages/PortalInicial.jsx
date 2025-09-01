4092f92218be5c64332ddee430bcae13d2be479b
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-01 14:22:55 -0300

    refactor: refatornado pagina inicial

--- src/pages/PortalInicial.jsx
+++ src/pages/PortalInicial.jsx
@@ -7,9 +7,12 @@ export const PortalInicial = () => {
   return (
     <div>
    <Header1/>
-   <section className='bg-amber-400 flex flex-col items-center p-20'>
-    <p>P</p>
-    <p>Assista a Matéria</p>
+   <section className='bg-amber-400 flex flex-col items-start gap-6 p-15'>
+    <h2 className='text-4xl '>Brasil luta, mas é <br/> derrotado na final <br/> contra os EUA</h2>
+    <button>Saiba Mais</button>
+   </section>
+   <section>
+
    </section>
     </div>
   )
