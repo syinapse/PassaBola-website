576e678386a912a539656f7fba6a51bfd4c2ddf9
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-04 20:00:16 -0300

    feat:fim esquelto footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,27 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>
-      <div className='bg-background-3 text-white'>
-      <div></div>
-      <div>
-        <h2>Redes Socias</h2>
-        <div>
-      <p></p>
-        </div>
-      </div>
+    <footer className='bg-background-3 text-white'>
+      <div className='  flex items-center h-55 p-10 '>
+        <div className='flex justify-around w-1/2'> 
       <div>LOGO</div>
+      <div className='flex flex-col gap-2'>
+        <h2>Redes Socias</h2>
+        <nav>
+      <p>@passaabola</p>
+      <p>@passaabola</p>
+      <p>@passaabola</p>
+        </nav>
       </div>
+      <address className='flex flex-col items-center'>
+        <p>Contato</p>
+        <p>contato@passabola.com.br</p>
+        <button>FALE CONOSCO</button>
+      </address>
     </div>
+   
+      </div>
+       <p className=' text-sm p-10 '>© Copyright 2025. PassaaBola. Todos os direitos reservados</p>
+      </footer>
   )
 }
