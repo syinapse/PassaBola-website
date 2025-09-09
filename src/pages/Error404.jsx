2fe40695873b42318afdee7665de5b9f745a3777
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-09 19:14:30 -0300

    feat: pagina de error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Header1 } from '../components/Header1'
+import { useNavigate } from 'react-router-dom'
 
 export const Error404 = () => {
+  const navigate = useNavigate()
   return (
-    <div>Error404</div>
+    <div className="overflow-hidden min-h-screen flex flex-col bg-background">
+      <Header1 />
+      <div className="flex flex-1 flex-col items-center justify-center text-center gap-4">
+        <p >404</p>
+        <h2 className="text-5xl text-primary font-bold">Página Não Encontrada</h2>
+        <p className="text-gray-600 max-w-md">
+          A página acessada não existe ou foi movida para outro local.
+        </p>
+        <button onClick={() => navigate('/')}
+         className="mt-4 px-6 py-2 rounded-xl bg-primary text-white font-semibold ">
+          Ir para Início
+        </button>
+      </div>
+    </div>
   )
 }
