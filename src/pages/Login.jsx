45b55ca3c91d33d2b9d8decf3301630ccbcf2d0d
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-10 19:58:05 -0300

    feat: criacao da logica de login usuario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { Header3 } from '../components/Header3'
 
 export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   return (
+    
     <div>
         <Header3/>
     </div>
