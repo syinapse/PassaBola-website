bf21cbd72d37d6dd8d0095f0bcdbef64ea7f36eb
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-14 12:12:43 -0300

    feat: area de cards

--- src/pages/Loja.jsx
+++ src/pages/Loja.jsx
@@ -9,6 +9,32 @@ export const Loja = () => {
     <h2 className='text-4xl text-white'>3 CAMISETAS PASSE A BOLA</h2>
     <p className='text-3xl text-primary-2'>POR R$299,90</p>
 </section>
+<div className="flex flex-col items-center justify-center py-12 bg-background">
+
+  <h2 className="text-3xl mb-8">Mais Vendidos</h2>
+
+
+  <section className="w-full max-w-6xl px-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+
+<div className="rounded-xl shadow-xl shadow-gray-400/50 p-4 flex flex-col items-center">
+<h2>PRODUTAOOO</h2>
+  <div className="w-full border-t border-gray-400 my-3"></div>
+  <p className=" font-medium text-md">Camiseta 1 Flamengo Fem</p>
+  <p className="text-primary font-bold text-lg">R$ 399,99</p>
+</div>
+
+
+    </div>
+
+
+    <div className="flex justify-center mt-8">
+      <button className=" text-primary border-primary border-2 px-6 py-2 rounded-lg font-semibold text-lg shadow">
+        Ver tudo
+      </button>
+    </div>
+  </section>
+</div>
 
     </div>
   )
