f3198ec76b84797f1409c09eafc4b572cd7bb91f
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-14 13:25:54 -0300

    feat: final footer completo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import logo from '../assets/logos/LogoPassaabola.svg'
+import insta from '../assets/icons/instagramLogo.svg'
+import youtube from '../assets/icons/youtubeLogo.svg'
+import twiter from '../assets/icons/twiterLogo.svg'
+import email from '../assets/icons/Email.svg'
+
 
 export const Footer = () => {
   return (
@@ -10,26 +15,26 @@ export const Footer = () => {
         <div className='flex flex-col md:flex-row w-full md:w-1/2 gap-6 items-center md:items-start justify-center md:justify-between'>
           <img src={logo} alt="Logo" className="w-36 h-30 object-contain" />
           <div className='flex flex-col gap-2 items-center md:items-start text-center md:text-left'>
-            <h2>Redes Socias</h2>
+            <h2 className='text-xl'>Redes Socias</h2>
             <nav>
-              <p>@passaabola</p>
-              <p>@passaabola</p>
-              <p>@passaabola</p>
+              <p className='text-gray-400 flex items-center gap-1'><img src={insta} className='w-4 h-4 object-contain' alt="instagram" /> @passaabola</p>
+              <p className='text-gray-400 flex items-center gap-1'><img src={youtube} className='w-4 h-4 object-contain' alt="youtube" /> @passaabola</p>
+              <p className='text-gray-400 flex items-center gap-1'><img src={twiter} className='w-4 h-4 object-contain' alt="twiter" /> @passaabola</p>
             </nav>
           </div>
 
-          <address className='flex flex-col items-center md:items-start'>
-            <p>Contato</p>
-            <p>contato@passaabola.com.br</p>
-            <button>FALE CONOSCO</button>
-          </address>
+          <div className='flex flex-col gap-2 items-center md:items-start'>
+            <p className='text-xl'>Contato</p>
+            <p className='text-gray-400 flex items-center gap-1'><img src={email} className='w-4 h-4 object-contain' alt="email" /> contato@passabola.com.br</p>
+            <button className='bg-btn-accent rounded-xl px-4 py-2 font-bold cursor-pointer'>FALE CONOSCO</button>
+          </div>
 
         </div>
       </div>
 
 
-<p className='text-sm p-10 relative'>
-  <span className="block border-t border-gray-500 w-full md:hidden mb-2"></span>
+<p className='text-sm p-10 relative text-gray-300'>
+  <span className="block border-t  border-gray-500 w-full md:hidden mb-2"></span>
   © Copyright 2025. PassaaBola. Todos os direitos reservados
 </p>
     </footer>
