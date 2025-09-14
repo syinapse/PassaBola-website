5f41524cd2ac2838421d0d71875052c22bad83da
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-14 13:47:14 -0300

    feat: terminado pagina de erro 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { Header1 } from '../components/Header1'
 import { useNavigate } from 'react-router-dom'
 import { Header3 } from '../components/Header3'
-
+import logo from '../assets/logos/LogoPassaabola.svg'
 export const Error404 = () => {
   const navigate = useNavigate()
   return (
     <div className="overflow-hidden min-h-screen flex flex-col bg-background">
+<img 
+  src={logo} 
+  alt="Logo Fundo" 
+  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-10 w-3/4 h-auto pointer-events-none"
+/>
+
       <Header3/>
       <div className="flex flex-1 flex-col items-center justify-center text-center gap-4">
         <p >404</p>
