4f04e9ee4dc61e65c736bfe2966ce884457eecc2
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-14 13:48:49 -0300

    refactor: ajusatndo img

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -7,12 +7,12 @@ export const Error404 = () => {
   const navigate = useNavigate()
   return (
     <div className="overflow-hidden min-h-screen flex flex-col bg-background">
-<img 
-  src={logo} 
-  alt="Logo Fundo" 
-  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-10 w-3/4 h-auto pointer-events-none"
-/>
-
+          <img 
+        src={logo} 
+        alt="Logo Fundo" 
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-10 w-3/7 h-auto pointer-events-none"
+      />
+      
       <Header3/>
       <div className="flex flex-1 flex-col items-center justify-center text-center gap-4">
         <p >404</p>
