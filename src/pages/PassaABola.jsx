58256844f15f4e3a0af293a5f7e0cc840c29e6b8
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-15 14:37:34 -0300

    feat: colocando header e footer

--- src/pages/PassaABola.jsx
+++ src/pages/PassaABola.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Header1 } from '../components/Header1'
+import { Footer } from '../components/Footer'
 
 export const PassaABola = () => {
   return (
-    <div>PassaABola</div>
+    <div>
+        <Header1/>
+        <Footer/>
+    </div>
   )
 }
