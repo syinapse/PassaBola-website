164f764b18b2953bd99ecc77c153a4f131e55142
Vitor <vitramosf@gmail.com>
2025-09-14 21:15:25 -0300

    Alterando tamanho da fonte

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -15,10 +15,10 @@ export const NewsCard = ({ imageSrc, title, date }) => {
       <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent transition-opacity duration-300 group-hover:opacity-80"></div>
 
       <div className="relative z-10 p-6 flex flex-col justify-end h-full text-white">
-        <h3 className="text-2xl font-bold leading-tight font-quando"> 
+        <h3 className="text-3xl font-bold leading-tight font-quando"> 
           {title}
         </h3>
-        <p className="text-sm mt-2 opacity-90 font-quando">
+        <p className="text-lg mt-2 opacity-90 font-quando">
           {date}
         </p>
       </div>
