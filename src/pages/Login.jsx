a2a6127d30cd73148ccaf59250e9a6fa3b377faf
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-15 15:18:31 -0300

    feat: final tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { Header3 } from '../components/Header3'
 import logoGoogle from '../assets/logos/googleLogo.svg'
 import videoBg from '../assets/banners/videoBanner.mp4'
+
+import email from '../assets/icons/emailBlack.svg'
+import eyesIcon from '../assets/icons/eyesIcon.svg'
+import passwordIcon from '../assets/icons/passwordIcon.svg'
 export const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -43,15 +47,15 @@ export const Login = () => {
        <div className='w-full md:w-1/2 flex flex-col items-center justify-start pt-8 md:pt-20 gap-8 min-h-screen bg-background'>
               {/*lado direito da tela de ligin*/}
         <div className='flex flex-col items-start ml-8 md:ml-20 lg:ml-40 px-8 w-full gap-3'>
-              <h2 className='text-3xl'>Login <span className='text-text-accent'>PASSA A BOLA</span></h2>
+              <h2 className='text-4xl font-quando'>Login <span className='text-text-accent font-oswald text-3xl'>PASSA A BOLA</span></h2>
               <button className='bg-white p-2 rounded-2xl border-1 flex gap-1 border-gray-300 font-semibold cursor-pointer'><img src={logoGoogle} className="w-6 h-6 object-contain"  alt="google" /> Entre com Google</button>
           <hr className="border-t border-gray-400 w-2xl my-4" />
             </div>
-<div className="flex flex-col gap-4 w-full max-w-md">
+<div className="flex flex-col gap-4 w-full max-w-md font-medium">
   <div className="flex flex-col">
     <p className="mb-1">Email</p>
     <div className="flex items-center border-2 border-secondary-2 rounded gap-2 px-2">
-      <p>O</p>
+      <img src={email} className='w-5 h-5 object-contain' alt='email'/>
       <input
         type="text"
         placeholder="exemplo@email.com"
@@ -65,7 +69,7 @@ export const Login = () => {
   <div className="flex flex-col">
     <p className="mb-1">Senha</p>
     <div className="flex items-center border-2 border-secondary-2  rounded gap-2 px-2">
-      <p>O</p>
+           <img src={passwordIcon} className='w-6 h-5 object-contain' alt='passwordIcon'/>
       <input
         type="password"
         placeholder="minhasenha1234"
@@ -73,7 +77,7 @@ export const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
         className="flex-1 outline-none p-2"
       />
-      <p>O</p>
+          <img src={eyesIcon} className='w-6 h-6 object-contain' alt='eyesIcon'/>
     </div>
   </div>
   <p className='ml-auto text-gray-400 font-medium italic'>Esqueci minha senha...</p>
